5444aa7cf9bcb739ca067ab50bd6d4a9d75c9e81
williycole <64283810+williycole@users.noreply.github.com>
2021-07-16 00:08:41 -0500

    Fetch and Random Generation Fix
    Got the fetch and random generation working, next I need to implement the random generation with the submit button.

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,24 +1,27 @@
 import { useEffect, useState } from "react";
 const Quote = () => {
-  const [quote, setQuote] = useState("unloaded");
-  const [author, setAuthor] = useState("unloaded");
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
 
   useEffect(() => {
     fetchData();
   }, []);
 
-  const fetchData = () => {
-    return fetch(
+  async function fetchData() {
+    const randomizer = Math.floor(Math.random() * (100 - 1 + 1)) + 1; // props maybe from parent component? need to use with button to generate
+    const res = await fetch(
       "https://gist.githubusercontent.com/natebass/b0a548425a73bdf8ea5c618149fe1fce/raw/f4231cd5961f026264bb6bb3a6c41671b044f1f4/quotes.json"
-    )
-      .then((response) => response.json())
-      .then((data) => console.log(data));
-  };
-  //   console.log(data);
+    );
+
+    const json = await res.json();
+    setAuthor(json[randomizer].author);
+    setQuote(json[randomizer].quote);
+  }
+
   return (
     <div>
-      <h1 id="text">"test"</h1>
-      <h2 id="author">"test"</h2>
+      <h1 id="text">{author}</h1>
+      <h2 id="author">{quote}</h2>
     </div>
   );
 };
