9c6e3588934f1928cff9a4a0714f2a5d37ff6e99
williycole <64283810+williycole@users.noreply.github.com>
2021-07-18 19:46:31 -0500

    Backing up before styling
    Added in techy api for the random quotes and kept the hold api and randomizer approach as a back up and a reference.

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -8,20 +8,32 @@ const Quote = () => {
   }, []);
 
   async function fetchData() {
-    const randomizer = Math.floor(Math.random() * (100 - 1 + 1)) + 1; // props maybe from parent component? need to use with button to generate
+    // Backup incase other api has trouble in the future
+    // const randomizer = Math.floor(Math.random() * (100 - 1 + 1)) + 1; // props maybe from parent component? need to use with button to generate
+
     const res = await fetch(
-      "https://gist.githubusercontent.com/natebass/b0a548425a73bdf8ea5c618149fe1fce/raw/f4231cd5961f026264bb6bb3a6c41671b044f1f4/quotes.json"
+      //Backup incase other api has trouble in the future
+      // "https://gist.githubusercontent.com/natebass/b0a548425a73bdf8ea5c618149fe1fce/raw/f4231cd5961f026264bb6bb3a6c41671b044f1f4/quotes.json"
+      "http://quotes.stormconsultancy.co.uk/random.json"
     );
 
     const json = await res.json();
-    setAuthor(json[randomizer].author);
-    setQuote(json[randomizer].quote);
+    console.log(json);
+    // Backup incase other api has trouble in the future
+    // setAuthor(json[randomizer].author);
+    // setQuote(json[randomizer].quote);
+
+    setAuthor(json.author);
+    setQuote(json.quote);
   }
 
   return (
     <div>
       <h1 id="text">{author}</h1>
       <h2 id="author">{quote}</h2>
+      <button id="new-quote" onClick={fetchData}>
+        Get New Quote
+      </button>
     </div>
   );
 };
--- src/components/QuoteBox.js
+++ src/components/QuoteBox.js
@@ -4,10 +4,10 @@ const QuoteBox = (props) => {
   return (
     <div id="quote-box">
       <Quote />
+
       <a id="tweet-quote" href="twitter.com/intent/tweet">
         <p>Tweet Me</p>
       </a>
-      <button id="new-quote">Click Me</button>
     </div>
   );
 };
